7ab02c3bd11ce9595fd7ef4700d01e2060210824
Sooraj Hamirani <hamiranisooraj@gmail.com>
2024-12-21 02:32:33 +0500

    debug the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,10 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='flex-item-center justify-between py-5 font-medium'>
+    <div className='flex flex-item-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-36 ' alt="" />
-      <ul className='hidden sm:flex gap-5 text-sm text-grey-700'>
+
+      <ul className='sm:flex gap-5 text-sm text-grey-700'>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
           <p>HOME</p>
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700' />
--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Collection = () => {
   return (
-    <div>Collection</div>
+    <div>this is collection</div>
   )
 }
 
